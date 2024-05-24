7fd7670fba04c3b1a2168d6f10b701a1c45459f2
atabongange23 <atabongangei046@gmail.com>
2024-05-24 14:17:44 +0100

    adding the display book api

--- backend/app.py
+++ backend/app.py
@@ -8,15 +8,22 @@ def display():
 
 @app.route("/books", methods=["GET"])
 def display():
-    pass
+    books = Books.query.all()
+    json_books = [book.to_json() for book in books]
+    return jsonify({"books": json_books})
 
 @app.route("/create_book", methods=["POST"])
 def create_book():
     pass
 
-@app.route("/update_book", methods=["POST"])
-def create_book():
+@app.route("/update_book", methods=["put"])
+def update_book():
     pass
+
+@app.route("/update_book", methods=["DELETE"])
+def update_book():
+    pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
